9e8ec3cadeb0aad3ba2e2475f20a52482f0466b6
42atlas <andraffaelli@gmail.com>
2022-08-16 16:39:40 +0200

    mongodb connection

---
+++ pages/api/seed.js
@@ -0,0 +1,13 @@
+import User from '../../models/Users'
+import data from '../../utils/data'
+import db from '../../utils/db'
+
+const handler = async (req, res) => {
+  await db.connect()
+  await User.deleteMany()
+  await User.insertMany(data.users)
+  await db.disconnect()
+  res.send({ message: 'Seeded successfully' })
+}
+
+export default handler
