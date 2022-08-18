4c38d80c74d3d1e8021bb8d84b8adfcd239d2f8e
42atlas <andraffaelli@gmail.com>
2022-08-18 13:28:28 +0200

    product model + seed api

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,4 @@
+import Product from '../../models/Products'
 import User from '../../models/Users'
 import data from '../../utils/data'
 import db from '../../utils/db'
@@ -6,6 +7,8 @@ const handler = async (req, res) => {
   await db.connect()
   await User.deleteMany()
   await User.insertMany(data.users)
+  await Product.deleteMany()
+  await Product.insertMany(data.products)
   await db.disconnect()
   res.send({ message: 'Seeded successfully' })
 }
