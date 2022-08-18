4828c47208f79d8d611a3725492910a2376f69b3
42atlas <andraffaelli@gmail.com>
2022-08-18 13:12:06 +0200

    payment screen + global.css+ store.js

--- utils/Store.js
+++ utils/Store.js
@@ -51,6 +51,15 @@ function reducer(state, action) {
           },
         },
       }
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      }
+
     default:
       return state
   }
