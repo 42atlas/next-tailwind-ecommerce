db9b8d8d17e9dc0c2f0697689fb727b781ab5a67
42atlas <andraffaelli@gmail.com>
2022-08-20 14:39:02 +0200

    axios connection to database

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,28 +1,30 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/Layout'
 import { useRouter } from 'next/router'
-import data from '../../utils/data'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Store } from '../../utils/Store'
+import Product from '../../models/Products'
+import db from '../../utils/db'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const { product } = props
   const { state, dispatch } = useContext(Store)
   const router = useRouter()
-  const { query } = useRouter()
-  const { slug } = query
-  const product = data.products.find((x) => x.slug === slug)
+
   if (!product) {
-    return <div>Product Not Found</div>
+    return <Layout title='Product Not Found'>Product Not Found</Layout>
   }
 
-  const addToCartHandler = () => {
+  const addToCartHandler = async () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug)
     const quantity = existItem ? existItem.quantity + 1 : 1
+    const { data } = await axios.get(`/api/products/${product._id}`)
 
-    if (product.countInStock < quantity) {
-      alert('Sorry, Product is out of stock')
-      return
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry, Product is out of stock')
     }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } })
     router.push('/cart')
@@ -77,4 +79,18 @@ export default function ProductScreen() {
       </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context
+  const { slug } = params
+
+  await db.connect()
+  const product = await Product.findOne({ slug }).lean()
+  await db.disconnect()
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  }
 }
