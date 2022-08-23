9704c1afe7e910009b8240078034b939361088d9
42atlas <andraffaelli@gmail.com>
2022-08-23 11:53:55 +0200

    23 order screen // register not working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,21 @@
-import NextAuth from 'next-auth/next'
+import bcryptjs from 'bcryptjs'
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import User from '../../../models/User'
 import db from '../../../utils/db'
-import bcryptjs from 'bcryptjs'
-import CredentialsProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
   session: {
     strategy: 'jwt',
   },
   callbacks: {
-    async jwt({ user, token }) {
+    async jwt({ token, user }) {
       if (user?._id) token._id = user._id
       if (user?.isAdmin) token.isAdmin = user.isAdmin
       return token
     },
     async session({ session, token }) {
-      if (token?._id) session.user._Id = token._id
+      if (token?._id) session.user._id = token._id
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin
       return session
     },
@@ -24,7 +24,9 @@ export default NextAuth({
     CredentialsProvider({
       async authorize(credentials) {
         await db.connect()
-        const user = await User.findOne({ email: credentials.email })
+        const user = await User.findOne({
+          email: credentials.email,
+        })
         await db.disconnect()
         if (user && bcryptjs.compareSync(credentials.password, user.password)) {
           return {
@@ -35,7 +37,7 @@ export default NextAuth({
             isAdmin: user.isAdmin,
           }
         }
-        throw new Error('Invalid credentials')
+        throw new Error('Invalid email or password')
       },
     }),
   ],
--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,11 @@
-import { signIn, useSession } from 'next-auth/react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
+import { signIn, useSession } from 'next-auth/react'
 import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
 import Layout from '../components/Layout'
 import { getError } from '../utils/error'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
+import axios from 'axios'
 
 export default function LoginScreen() {
   const { data: session } = useSession()
@@ -22,10 +22,17 @@ export default function LoginScreen() {
   const {
     handleSubmit,
     register,
+    getValues,
     formState: { errors },
   } = useForm()
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name,
+        email,
+        password,
+      })
+
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -39,12 +46,28 @@ export default function LoginScreen() {
     }
   }
   return (
-    <Layout title='Login'>
+    <Layout title='Create Account'>
       <form
         className='mx-auto max-w-screen-md'
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className='mb-4 text-xl'>Login</h1>
+        <h1 className='mb-4 text-xl'>Create Account</h1>
+        <div className='mb-4'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            className='w-full'
+            id='name'
+            autoFocus
+            {...register('name', {
+              required: 'Please enter name',
+            })}
+          />
+          {errors.name && (
+            <div className='text-red-500'>{errors.name.message}</div>
+          )}
+        </div>
+
         <div className='mb-4'>
           <label htmlFor='email'>Email</label>
           <input
@@ -58,7 +81,6 @@ export default function LoginScreen() {
             })}
             className='w-full'
             id='email'
-            autoFocus
           ></input>
           {errors.email && (
             <div className='text-red-500'>{errors.email.message}</div>
@@ -70,25 +92,44 @@ export default function LoginScreen() {
             type='password'
             {...register('password', {
               required: 'Please enter password',
-              minLength: {
-                value: 6,
-                message: 'Password must have more than 5 chars',
-              },
+              minLength: { value: 6, message: 'password is more than 5 chars' },
             })}
             className='w-full'
             id='password'
             autoFocus
           ></input>
           {errors.password && (
-            <div className='text-red-500'>{errors.password.message}</div>
+            <div className='text-red-500 '>{errors.password.message}</div>
           )}
         </div>
         <div className='mb-4'>
-          <button className='primary-button'>Login</button>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input
+            className='w-full'
+            type='password'
+            id='confirmPassword'
+            {...register('confirmPassword', {
+              required: 'Please enter confirm password',
+              validate: (value) => value === getValues('password'),
+              minLength: {
+                value: 6,
+                message: 'confirm password is more than 5 chars',
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <div className='text-red-500 '>
+              {errors.confirmPassword.message}
+            </div>
+          )}
+          {errors.confirmPassword &&
+            errors.confirmPassword.type === 'validate' && (
+              <div className='text-red-500 '>Password do not match</div>
+            )}
         </div>
-        <div className='mb-4'>
-          Don&apos;t have an account? &nbsp;
-          <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
+
+        <div className='mb-4 '>
+          <button className='primary-button'>Register</button>
         </div>
       </form>
     </Layout>
